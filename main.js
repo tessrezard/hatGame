b34ae9ae09201f5d69c406554e1a6c83e5ce9264
Tess Rezard <tessrezard@Jarvis-2.local>
2023-08-01 09:48:50 +0100

    generate field works, but play is not workng on generated field

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const pathCharacter = '*';
 
 class Field {
     constructor(grid){
-      this.grid = grid;
+        this.grid = grid;
     }
 
     print() {
@@ -17,44 +17,24 @@ class Field {
         }
     }
 
-    //essentially, what we are doing, is changing things into stars. 
-    //the user is prompted : to 'move' the star. 
-    //this will mean changing one of the elements if the arrays of the grid to a * ie 'pathCharacter'
-    // if the element to be changed if a O ie 'hole' or is outside of the array, then LOSE 
-    // if the element is a ░ ie 'fieldCharacter, change to a *. 
-    // if the element to be changed is the 'hat' ^ , then WIN!
-    // TO DO : 
-    // make method to ask the user or input
-    // make a method that checks how that input affects the grid
-    // add input to grid 
-    // state win or loss
-
     input() {
-        // we need to keep asking fro input until the star is on the hat
-        // need ot define a how to move. 
-
-        // check if the user has won. 
-        // first find the hat. scope. need to remember where the hat WAS even when it ihas changed to a star. 
         let hatIndex;
         let hatRow;
         let move;
         //set initial player posititon
-        // position = [row, collumn]
+            // position = [row, collumn]
         let position = [0, 0];
+        //find hat
         for (let i = 0; i < this.grid.length; i++){
             if (this.grid[i].indexOf(hat) != -1){
                 hatRow = i;
                 hatIndex = this.grid[i].indexOf(hat);
             }
         }
-
-        // then find all the stars. if any of the stars are in the same place as the hat? 
-        //check that the hat position is not now a star, is still the hat. 
+        //check that the hat position is still the hat. 
             // if yes, print 'you won'
             // else, prompt move
-       
         while (this.grid[hatRow][hatIndex] === hat ){
-
             myField.print();
             move = prompt("Which way? ( l, r, u, d ) ");
             // set current position
@@ -72,6 +52,7 @@ class Field {
                     position[0]++;
                     break;
                 }
+            //check if move made user lose
             if (this.grid[position[0]][position[1]] ===hole){
                 console.log('Game over! You fell in a hole!');
                 return ('Game over! You fell in a hole!');
@@ -80,35 +61,81 @@ class Field {
                 console.log(('Game over! Out of bounds!'));
                 return ('Game over! out of bounds!');
             }
+            //valid move, mutate arrays to splice a pathCharacter(star)
             this.grid[position[0]].splice(position[1], 1, pathCharacter);
-            
         }
         if (this.grid[hatRow][hatIndex] != hat){
             console.log('Congrats! You got your hat! YOU WON!'); 
+            return ('Congrats! You got your hat! YOU WON!');
         }
-
-
-
-        // when move made, mutate arrays to sawp a star
-
     }
 
+
+    //returns a randomized two-dimensional array representing the field, inc 1 hat & >2 holes, takes arguments for height and width, and percentage of field covered by holes.
+     static generateField(height, width, percentage) {
+        console.log('in gen field');
+        const field = [];
+        //buid an base field to size 
+        while (field.length < height){
+            const row = []; 
+            while (row.length < width){
+                row.push(fieldCharacter);
+            }
+            field.push(row);
+            // console.log(row);
+        }
+        // //asign pathCharacter to top left
+            field[0][0] = pathCharacter;
+        // assign one hat to random place 
+        let randomRow = 0;
+        let randomCol = 0;
+        while (randomRow === 0 && randomCol === 0){
+                randomRow = Math.floor(Math.random() * height);
+                randomCol = Math.floor(Math.random() * width);
+            }
+        field[randomRow][randomCol] = hat;
+        hatX = randomCol;
+        hatY = randomRow;
+        //assign the holes. there should be the percentage of height * width
+        let numHoles = ((height * width)/100)*percentage;
+        console.log(numHoles);
+        //keep track of the holes
+        let holesLocations = [];
+        for (let i = 0; i <= numHoles; i++){
+            let holeLocation = [0, 0];
+            let randomRow = 0;
+            let randomCol = 0;
+            while (randomRow === 0 && randomCol === 0 || field[randomRow][randomCol] === hole || field[randomRow][randomCol] === hat){
+                    randomRow = Math.floor(Math.random() * height);
+                    randomCol = Math.floor(Math.random() * width);
+            }
+            holeLocation = [randomRow, randomCol];
+            field[randomRow][randomCol] = hole;
+            holesLocations.push(holeLocation);
+        }
+        
+        for (const row in field) {
+            console.log(field[row].join(''));
+        }
+    }
 }
 
+const myfield = new Field(Field.generateField(30, 20, 4));
+myfield.input();
 
-const myField = new Field([
-    ['*', '░', '░', 'O', '░', '░'],
-  ['░','░', 'O', '░', '░', '░'],
-  ['░','░', '░', 'O', '░', '░'],
-  ['░','O', '░', '░', '░', '░'],
-  ['░','░', '░', 'O', '░', '░'],
-  ['░','░', '^', '░', '░', '░'],
-  ['░','░', 'O', '░', '░', '░'],
-  ['░','░', '░', 'O', '░', '░'],
+// const myField = new Field([
+//     ['*', '░', '░', 'O', '░', '░'],
+//   ['░','░', 'O', '░', '░', '░'],
+//   ['░','░', '░', 'O', '░', '░'],
+//   ['░','O', '░', '░', '░', '░'],
+//   ['░','░', '░', 'O', '░', '░'],
+//   ['░','░', '^', '░', '░', '░'],
+//   ['░','░', 'O', '░', '░', '░'],
+//   ['░','░', '░', 'O', '░', '░'],
   
-]);
+// ]);
 
 
 
-myField.input();
+
 
